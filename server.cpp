a2c9496f961ccfb0544b9e0f186ccd1f3086b337
unnsteinngardars <unnsteinng@gmail.com>
2018-09-23 22:36:47 +0000

    send to all and username

--- server.cpp
+++ server.cpp
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <sstream>
 #include <string.h>
+#include <string>
 #include <vector>
 #include <map>
 #include <sys/socket.h>
@@ -9,13 +11,27 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <stdexcept>
 
 /* GLOBAL VARIABLES */
 int server_port = 5923;
 int MAX_CONNECTIONS_FOR_LISTEN_QUEUE = 10;
 int MAX_BYTES = 256;
 std::string welcome_message = "Welcome to LICKING LEMON LOLLIPOPS IN LILLEHAMMER chat server\n\nType HELP to display available commands\n\n";
-std::map<int, std::string> users;
+std::string help_message = "List of options";
+
+std::map<int, std::string> users_by_fd;
+// std::map<char *, int> users_by_name;
+
+int server_socket_fd;
+
+int max_file_descriptor;
+
+/* amount of bytes read from read/recv and write/send will be stored in this variable */
+int read_bytes, write_bytes;
+
+/* file descriptor sets, active holds all currently connected fds along with the listenin fd */
+fd_set active_set, read_set;
 
 /* COMMANDS */
 const std::string ID = "ID";
@@ -96,12 +112,117 @@ void listen(int socketfd)
 	}
 }
 
+const char *getUserNameByFd(int fd)
+{
+	const char *username;
+	try
+	{
+		username = users_by_fd.at(fd).c_str();
+	}
+	catch (const std::out_of_range &e)
+	{
+		return NULL;
+	}
+	printf("username: %s\n", username);
+	return username;
+	// std::map<int, const char *>::iterator it;
+	// it = users_by_fd.find(fd);
+	// if (it == users_by_fd.end())
+	// {
+	// 	return NULL;
+	// }
+	// return it->second;
+	// return users_by_fd.find(fd)->second;
+}
+
+inline std::string trim_right_copy(
+	const std::string &s,
+	const std::string &delimiters = " \f\n\r\t\v")
+{
+	return s.substr(0, s.find_last_not_of(delimiters) + 1);
+}
+
+int getFdByUserName(char *username)
+{
+	int fd = -1;
+	std::string username_to_find = std::string(trim_right_copy(username));
+	printf("username to look for: %s\n", username_to_find.c_str());
+
+	std::map<int, std::string>::iterator it;
+	for (it = users_by_fd.begin(); it != users_by_fd.end(); ++it)
+	{
+		printf("FD: %d USER: %s\n", it->first, it->second.c_str());
+		if (!it->second.compare(username_to_find))
+		{
+			printf("user %s found!\n", it->second.c_str());
+			fd = it->first;
+		}
+	}
+	return fd;
+	// return users_by_name.find(username)->second;
+}
+
+/**
+ * Send message in body to all clients
+*/
+void sendToAll(char *body, int current_fd)
+{
+	// Check if current_fd has corresponding user name
+	// const char *username = getUserNameByFd(current_fd);
+
+	int length = strlen(body);
+	char buffer[length];
+	memset(buffer, 0, length);
+	memcpy(buffer, body, length);
+	printf("length: %d", length);
+
+	for (int j = 0; j <= max_file_descriptor; j++)
+	{
+		/* Check if the client is in the active set */
+		if (FD_ISSET(j, &active_set))
+		{
+			/* prevent the received message from the client from beeing sent to the server and himself */
+			if (j != server_socket_fd && j != current_fd)
+			{
+				write_bytes = write(j, buffer, length);
+				if (write_bytes < 0)
+				{
+					error("Sending to clients");
+				}
+			}
+		}
+	}
+}
+
+void sendToUser(char *body, int current_fd, int recv_fd)
+{
+	int length = strlen(body);
+	char buffer[length];
+	memset(buffer, 0, length);
+	memcpy(buffer, body, length);
+
+	if (FD_ISSET(recv_fd, &active_set))
+	{
+		write_bytes = write(recv_fd, buffer, length);
+		if (write_bytes < 0)
+		{
+			error("Sending to client");
+		}
+	}
+}
+
+void addUser(int fd, char *username)
+{
+	std::string username_to_add = std::string(trim_right_copy(username));
+	users_by_fd.insert(std::pair<int, std::string>(fd, username_to_add));
+}
+
 void parseCommand(char *buffer, int fd)
 {
 	char *command = strtok(buffer, " ");
-	char *nextCommand = strtok(NULL, " ");
+	char *next_command = strtok(NULL, " ");
 
-	printf("%s\n", command);
+	printf("command: %s, next_command: %s\n", command, next_command);
 
 	if (command == ID)
 	{
@@ -110,7 +231,11 @@ void parseCommand(char *buffer, int fd)
 	else if (command == CONNECT)
 	{
 		// SET USERNAME
-		users.insert(std::pair<int, std::string>(fd, nextCommand));
+		if (next_command != NULL)
+		{
+			printf("client with fd %d connecting with username %s", fd, next_command);
+			addUser(fd, next_command);
+		}
 	}
 	else if (command == LEAVE)
 	{
@@ -122,18 +247,41 @@ void parseCommand(char *buffer, int fd)
 	}
 	else if (command == MSG)
 	{
-		if (nextCommand == ALL)
+		if (next_command == ALL)
 		{
 			// SEND TO ALL
+			char *body = strtok(NULL, "");
+			const char *sending_user = getUserNameByFd(fd);
+			printf("username: %s fd: %d sending message to everyone\n", sending_user, fd);
+			sendToAll(body, fd);
 		}
 		else
 		{
-			// SEND TO NEXTCOMMAND USER
+			// SEND TO NEXTCOMMAND USER if exists
+			char *body = strtok(NULL, "");
+
+			// const char *sending_user = getUserNameByFd(fd);
+
+			int recipient = getFdByUserName(next_command);
+
+			// const char *recipient_user = getUserNameByFd(recipient);
+
+			printf("next_command in MSG ALL: %s\n", next_command);
+
+			if (recipient > 0)
+			{
+				sendToUser(body, fd, recipient);
+			}
+			else
+			{
+				write(fd, "abbababb\n", 9);
+			}
+			// else write feedback
 		}
 	}
 	else if (command == CHANGE)
 	{
-		if (nextCommand == ID)
+		if (next_command == ID)
 		{
 			// CHANGE ID
 		}
@@ -145,13 +293,15 @@ void parseCommand(char *buffer, int fd)
 	else
 	{
 		// INVALID COMMAND
+		write(fd, "Invalid command you fool!\n\n", 27);
 	}
+	printf("END OF COMMAND PARSER FUNCTION\n");
 }
 
 int main(int argc, char const *argv[])
 {
 	/* Variable declarations */
-	int server_socket_fd, new_server_socket_fd, server_port, max_file_descriptor;
+	int new_server_socket_fd, server_port;
 
 	/* server port */
 	/**
@@ -164,9 +314,6 @@ int main(int argc, char const *argv[])
 	/* the struct for the incomming client info */
 	struct sockaddr_in client;
 
-	/* file descriptor sets, active holds all currently connected fds along with the listenin fd */
-	fd_set active_set, read_set;
-
 	/* client_length is used for the accept call
 	 * http://pubs.opengroup.org/onlinepubs/009695399/functions/accept.html
 	 * Points to a socklen_t structure which on input specifies the length of the supplied sockaddr structure, and on output specifies the length of the stored address.
@@ -175,9 +322,6 @@ int main(int argc, char const *argv[])
 
 	/* buffer for messages */
 	char buffer[MAX_BYTES];
-
-	/* amount of bytes read from read/recv and write/send will be stored in this variable */
-	int read_bytes, write_bytes;
 
 	/* Create the socket for the server */
 	server_socket_fd = createAndBindSocket(server_port);
@@ -215,9 +359,17 @@ int main(int argc, char const *argv[])
 						error("Failed to establish connection");
 					}
 
-					printf("Connection established from host: %s, port: %d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+					printf("Connection established from host: %s, port: %d with fd: %d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port), new_server_socket_fd);
 
 					write(new_server_socket_fd, welcome_message.c_str(), welcome_message.length());
+
+					// /* Add user to user maps with anonymous name plus anonymous_counter*/
+					// std::string anonymous_counter_string = std::to_string(anonymous_counter);
+					// std::string anonymous_string = "anonymous" + anonymous_counter_string;
+					// const char *anonymous = anonymous_string.c_str();
+
+					// users_by_fd.insert(std::pair<int, const char *>(new_server_socket_fd, anonymous));
+					// users_by_name.insert(std::pair<const char *, int>(anonymous, new_server_socket_fd));
 
 					/* Add new connection to active set */
 					FD_SET(new_server_socket_fd, &active_set);
@@ -233,7 +385,7 @@ int main(int argc, char const *argv[])
 				{
 
 					/* Already established connection */
-					memset(buffer, 0, sizeof(buffer));
+					memset(buffer, 0, MAX_BYTES);
 					read_bytes = read(i, buffer, MAX_BYTES);
 
 					if (read_bytes < 0)
@@ -253,28 +405,27 @@ int main(int argc, char const *argv[])
 					else
 					{
 						// tjekka buffer og acta a rett command
+						printf("buffer received from client: %s\n", buffer);
 						parseCommand(buffer, i);
-
-						printf("Message: %s\n", buffer);
 
 						/* MSG ALL */
 						/* Loop through all connected clients */
-						for (int j = 0; j <= max_file_descriptor; j++)
-						{
-							/* Check if the client is in the active set */
-							if (FD_ISSET(j, &active_set))
-							{
-								/* prevent the received message from the client from beeing sent to the server and himself */
-								if (j != server_socket_fd && j != i)
-								{
-									write_bytes = write(j, buffer, read_bytes);
-									if (write_bytes < 0)
-									{
-										error("Sending to clients");
-									}
-								}
-							}
-						}
+						// for (int j = 0; j <= max_file_descriptor; j++)
+						// {
+						// 	/* Check if the client is in the active set */
+						// 	if (FD_ISSET(j, &active_set))
+						// 	{
+						// 		/* prevent the received message from the client from beeing sent to the server and himself */
+						// 		if (j != server_socket_fd && j != i)
+						// 		{
+						// 			write_bytes = write(j, buffer, read_bytes);
+						// 			if (write_bytes < 0)
+						// 			{
+						// 				error("Sending to clients");
+						// 			}
+						// 		}
+						// 	}
+						// }
 					}
 				}
 			}
